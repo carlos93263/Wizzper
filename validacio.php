70720db5a0a21d2e96c8fdd6e4cd7fa3f7c3dfe6
EnricGorriz <9074.joan23@jfe.edu>
2016-05-11 11:22:34 +0200

    sesions en un sol arxiu

---
+++ validacio.php
@@ -0,0 +1,26 @@
+<?php
+	session_start();
+		if(isset($_SESSION['error_login']))$error = $_SESSION['error_login'];
+		if(isset($_SESSION['validarse']))$error = $_SESSION['validarse'];
+	if(isset($_COOKIE["wizzpercookielogin"])){
+		$user = $_COOKIE["wizzpercookielogin"]; 
+		setcookie("wizzpercookielogin", $user, time()+60*60*24*30);//30 dies de vida de la cookie
+		$pagelog = strpos($_SERVER['PHP_SELF'], "login.php");
+		if($pagelog != 0){
+			header("Location: index.php");
+			die();
+		}
+	}
+	if(isset($error) || !isset($_SESSION["wizzperid"])){
+		if(isset($_COOKIE["wizzpercookielogin"])){
+			$user = $_COOKIE["wizzpercookielogin"];
+			setcookie("wizzpercookielogin", $user, time()-1);
+		}
+		session_destroy();
+		$pagelog = strpos($_SERVER['PHP_SELF'], "login.php");
+		if($pagelog == 0){
+			header("Location: login.php");
+			die();
+		}
+	}
+?>
